b2d9363cd25bafc438db82b17203d9fc1db9228e
MAHAFIZS <hafizsourav44@gmail.com>
2023-11-07 12:31:30 +0100

    Added setup.py for packaging the project as a Python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-quiz',  # Replace with your package name
+    version='1.0.0',   # Replace with your desired version
+    description='A math quiz game package',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
